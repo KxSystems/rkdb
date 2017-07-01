7d7df56058560c256885b7b4cd4b1943ff592911
statquant <statquant@statbox.default>
2017-07-01 23:52:16 +0100

    add tests add README directory

---
+++ README/README.R
@@ -0,0 +1,19 @@
+#' Check if a package is installed and install it if needs be
+#' 
+#' @name checkInstallAndLoad 
+#' @usage install packages if needs be to render the README
+checkInstallAndLoad <- function(packages){
+  for( package in packages){
+    if(! package %in% rownames(installed.packages())) install.packages(package)
+    library(package, character.only = TRUE)
+  }
+}
+
+# check and install packages necessary to render the README file
+checkInstallAndLoad(c('knitr', 'rmarkdown'))
+
+# render to html
+# rmarkdown::render(input='./README/README.Rmd', output_format='html_document')
+# render to md (default)
+rmarkdown::render(input='./README/README.Rmd', output_file = './README.md', intermediates_dir = './README')
+
