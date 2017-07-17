718283b8d476195f5f36e8164e35176f031371b2
Sergey Vidyuk <sergey@kx.com>
2017-07-17 15:58:49 +0100

    add example of table test

--- tests/testthat/test-tables.R
+++ tests/testthat/test-tables.R
@@ -1,6 +1,14 @@
 context("tables")
 
-test_that("multiplication works", {
-  h<-skip_unless_has_test_db()
-  expect_equal(2 * 2, 4)
+test_that("table shapes", {
+  h <- skip_unless_has_test_db()
+  col3K <- execute(h, "([]s:`a`b``1;e:``aa`bb`cc;qty:100 0N 0W -0Wi)")
+  col3R <-
+    data.frame(
+      s = c('a', 'b', '', '1'),
+      e = c('', 'aa', 'bb', 'cc'),
+      qty = c(100L, NA, as.integer(2147483647), -as.integer(2147483647)),
+      stringsAsFactors = FALSE
+    )
+  expect_equal(col3R, col3K)
 })
